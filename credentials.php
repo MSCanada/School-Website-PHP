995f10c4005e7c24e99ae4daf07d97098e28389b
gusanare1 <gang_emelec@yahoo.com>
2016-03-12 22:21:51 -0500

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,3 +1,4 @@
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -10,3 +11,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
+?>
